5cfbddeb231fd53903183e0cf3ba9811ce2e8e0f
tintheturtle <tinhoang@bu.edu>
2021-01-29 12:50:27 -0500

    added POST request for retrieving users in SQLAlchemy DB

--- project/server/users/views.py
+++ project/server/users/views.py
@@ -13,50 +13,47 @@ class UserAPI(MethodView):
     User Registration Resource
     """
 
-    def get(self):
+    def get(self):        
     	responseObject = {
     		'ping': 'pong',
     	}
     	return make_response(jsonify(responseObject)), 201
 
     def post(self):
-        # get the post data
-        post_data = request.get_json(); print(request)
-        # check if user already exists
-        user = User.query.filter_by(email=post_data.get('email')).first()
-        user = User.query
 
-        if not user:
+        # Get all users
+        users = User.query.filter().all()
+
+        # Create empty array
+        user_list = []
+
+        # Extract data from User Models
+        for user in users:
+            user_list += [ {"id": user.id, "email": user.email } ]
+
+        if not users:
+            # Empty List
+            responseObject = {
+                'status': 'success',
+                'message': 'Database is empty.'
+            }
+            return make_response(jsonify(responseObject)), 200
+        else:
             try:
-                user = User(
-                    email=post_data.get('email'),
-                    password=post_data.get('password')
-                )
-
-                # insert the user
-                db.session.add(user)
-                db.session.commit()
-                # generate the auth token
-                auth_token = user.encode_auth_token(user.id)
+                # User List is non-empty
                 responseObject = {
                     'status': 'success',
-                    'message': 'Successfully registered.',
-                    'auth_token': auth_token.decode()
+                    'message': 'Successfully queried database.',
+                    'user_list': user_list
                 }
                 return make_response(jsonify(responseObject)), 201
             except Exception as e:
+                # Error occurred
                 responseObject = {
                     'status': 'fail',
                     'message': 'Some error occurred. Please try again.'
                 }
                 return make_response(jsonify(responseObject)), 401
-        else:
-            responseObject = {
-                'status': 'fail',
-                'message': 'User already exists. Please Log in.',
-            }
-            return make_response(jsonify(responseObject)), 202
-
 
 # define the API resources
 user_view = UserAPI.as_view('user_api')
